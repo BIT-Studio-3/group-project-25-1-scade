bb9af257f7ad53df8ccbba4e0ae6eced0260f478
Joanna Marowa <marojm1@student.op.ac.nz>
2024-10-16 13:30:45 +1300

    set up contact page

--- disaster/disaster/src/routes/+page.svelte
+++ disaster/disaster/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<h1>Contact us </h1>
+
+<form action="">
+    <label for="name">Name
+        <input type="text" id="name">
+    </label>
+    <label for="email">Email
+        <input type="email" id="email">
+    </label>
+    <label for="message">Your message</label>
+    <textarea name="message" id="meddage"></textarea>
+</form>
+
+<style>
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
