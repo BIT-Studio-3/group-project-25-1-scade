cd491ee97e1b45a19558807df19f30639786c8c0
ZandarB <BEKEZA1@student.op.ac.nz>
2024-10-16 13:31:37 +1300

    skeleton for page

--- disaster/disaster/src/routes/+page.svelte
+++ disaster/disaster/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
+<!--import Footer from '$lib/Footer.svelte';-->
+<!--import Header from '../../lib/Header.svelte';-->
+<!--<Header/>-->
+<h1>About Page:</h1>
+<h2>About Us:</h2>
+<h3>Our Mission</h3>
+<p>To Give weather related information to various disaster response groups</p>
+<h3>Who We Are</h3>
+<p>We are a bunch of people trying to do something way above our paygrade <br> lol</p>
+<h3>What We Do</h3>
+<p>We do be trying to make weather. Science Yo!</p>
+<h3>Our Members</h3>
+<p>Photos here ig</p>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
