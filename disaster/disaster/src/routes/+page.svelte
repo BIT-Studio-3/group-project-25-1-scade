e92a6f36d92b8df3f11d11a48d9a5555d09b41ff
SaskiaBLittle <briesg1@student.op.ac.nz>
2024-10-16 14:27:18 +1300

    Adding an image component

--- disaster/disaster/src/routes/+page.svelte
+++ disaster/disaster/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
     import Header from '$lib/Header.svelte';
     import Footer from '$lib/Footer.svelte'
-
+    import Image from '$lib/Image.svelte';
+    import homepic from '$lib/Weather.jpg';
 </script>
 
 
@@ -10,7 +11,16 @@
 
 <p>This is the home page of our emergency weather alert application.</p>
 
+<div class="imageContainor">
+    <Image pic={homepic} picname={"cover"} />
+</div>
 
 
 <Footer year={2024} />
 
+<style>
+    .imageContainor {
+        display: flex;
+        justify-content: center;
+    }
+</style>
