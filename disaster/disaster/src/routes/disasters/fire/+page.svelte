82332aec02f3103cd1b6a3016c6646e6c93d6d01
arnoao1 <jedifishcake@gmail.com>
2024-10-23 14:31:32 +1300

    added images to disaster pages

--- disaster/disaster/src/routes/disasters/fire/+page.svelte
+++ disaster/disaster/src/routes/disasters/fire/+page.svelte
@@ -2,12 +2,16 @@
     import Header from '$lib/Header.svelte';
     import Footer from '$lib/Footer.svelte';
     import Image from '$lib/Image.svelte';
-    import homepic from '$lib/Weather.jpg';
+    import fire from '$lib/fire.jpg'
 </script>
 
 <Header headingTitle="Fires" />
 
 <p>Fires</p>
+
+<div class="imageContainor">
+    <Image pic={fire} picname={"cover"} picwidth={"600px"} picheight={"400px"}/>
+</div>
 
 <Footer year={2024} />
 
--- disaster/disaster/src/routes/disasters/flood/+page.svelte
+++ disaster/disaster/src/routes/disasters/flood/+page.svelte
@@ -2,12 +2,17 @@
     import Header from '$lib/Header.svelte';
     import Footer from '$lib/Footer.svelte';
     import Image from '$lib/Image.svelte';
-    import homepic from '$lib/Weather.jpg';
+    import flood from '$lib/flood.jpg'
 </script>
 
 <Header headingTitle="Floods" />
 
 <p>Flooding</p>
+
+<div class="imageContainor">
+    <Image pic={flood} picname={"cover"} picwidth={"600px"} picheight={"400px"}/>
+</div>
+
 
 <Footer year={2024} />
 
