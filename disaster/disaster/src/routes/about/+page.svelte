51a207e92dbef27f23190c0cbcdb6fb5f878b567
ZandarB <BEKEZA1@student.op.ac.nz>
2024-10-23 13:31:20 +1300

    changed some text

--- disaster/disaster/src/routes/about/+page.svelte
+++ disaster/disaster/src/routes/about/+page.svelte
@@ -9,11 +9,11 @@
 
 <h2>About Us:</h2>
 <h3>Our Mission</h3>
-<p>To Give weather related information to various disaster response groups</p>
+<p>To Give weather related information to various disaster response groups, to help them remain informed about what is happening at all times.</p>
 <h3>Who We Are</h3>
-<p>We are a bunch of people trying to do something way above our paygrade <br> lol</p>
+<p>We are a bunch of people working hard to bring you a working disaster API to help those who are helping others.</p>
 <h3>What We Do</h3>
-<p>We do be trying to make weather. Science Yo!</p>
+<p>We do be trying to make weather do the thing. Science Yo!</p>
 
 
 <div id="group">
