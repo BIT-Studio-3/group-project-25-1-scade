6e1c645de8594783220195195f53caaed994f677
Mapuhn1 <you@example.com>
2024-10-23 13:59:06 +1300

    layout for how to use page

--- disaster/disaster/src/routes/howToUse/+page.svelte
+++ disaster/disaster/src/routes/howToUse/+page.svelte
@@ -1,7 +1,27 @@
 <script>
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
 
 </script>
 
+<Header headingTitle="How To Use" />
+
+<article>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda quidem, libero praesentium eligendi perspiciatis incidunt maiores aliquam quis, aut quasi neque, dolores adipisci sequi odio nisi eveniet quas sapiente corrupti?</p>
+    <p>Quod iste non deleniti voluptas nobis ea rem explicabo, accusamus ipsum velit? Beatae eum quisquam ipsa illo, commodi impedit rem expedita facilis laborum reprehenderit alias cupiditate neque tenetur eius aspernatur!</p>
+    <p>Suscipit, quia, ducimus quaerat ad aut nesciunt eligendi fuga quasi modi debitis omnis quam voluptas laudantium doloremque sapiente? Exercitationem repellendus nam sint dolorem quod maiores architecto animi eius ratione rerum?</p>
+    <p>Veniam quis, inventore libero assumenda culpa ipsa odio! Voluptas iure, ratione ipsum fuga deleniti ducimus distinctio et quidem! Repellat sapiente doloribus a provident officia iusto sunt fugit at dolore veritatis.</p>
+    <p>Cum nulla eligendi, minima officia asperiores consequuntur ad ea error et ipsa illo ut fugiat eveniet ipsam beatae est quae perferendis laboriosam facere voluptatem maxime ab. Aut doloremque aliquam repellendus.</p>
+    <p>Numquam reprehenderit sint fuga ut autem esse hic dolores exercitationem possimus quod. Neque, quos qui ratione laboriosam dolor ipsa? Eos cupiditate necessitatibus illo voluptatibus, distinctio corporis magni placeat deleniti impedit.</p>
+</article>
+
+
+
+
+
+<Footer year={2024} />
+
 <style>
-    
+
+
 </style>
