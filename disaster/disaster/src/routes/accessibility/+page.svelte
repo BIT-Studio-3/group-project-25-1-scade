b872e36606e7ca2c8348be9da1420a59a3852bae
Mapuhn1 <you@example.com>
2024-10-23 14:31:50 +1300

    add footer and header to page

---
+++ disaster/disaster/src/routes/accessibility/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+
+</script>
+
+<Header headingTitle="Accessibility" />
+
+<Footer year={2024} />
+
+<style>
+
+    
+</style>
