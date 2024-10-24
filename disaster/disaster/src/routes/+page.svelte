2136f230cefa7f0809967825ac8593f26db5802f
arnoao1 <jedifishcake@gmail.com>
2024-10-25 11:34:49 +1300

    hopefully final pushing to main

--- disaster/disaster/src/routes/+page.svelte
+++ disaster/disaster/src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import Header from '$lib/Header.svelte';
     import Card from '$lib/Card.svelte';
     import Image from '$lib/Image.svelte';
+    import Footer from '$lib/Footer.svelte'
     import homepic from '$lib/Weather.jpg';
     let currentTime = new Date();
 
@@ -26,9 +27,6 @@
         </div>
     </div>
 </body> 
-
-<Footer year={2024} />
-
 <style>
     p{
         padding: 0.2%;
