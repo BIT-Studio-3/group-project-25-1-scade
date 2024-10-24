6ae0206be956548ea43909a58d69984fed42f9b6
SaskiaBLittle <briesg1@student.op.ac.nz>
2024-10-25 10:40:26 +1300

    current event page

---
+++ disaster/disaster/src/routes/current/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Header from '$lib/Header.svelte';
+    import Card from '$lib/Card.svelte';
+    import Footer from '$lib/Footer.svelte'
+    import Image from '$lib/Image.svelte';
+    import homepic from '$lib/Weather.jpg';
+    
+</script>
+
+
+<Header headingTitle="Current Events" />
+
+<h1>Dunedin Floods</h1>
+
+<!-- <Image pic={homepic} picname={"cover"} picwidth={"800px"} picheight={"400px"}/> -->
+
+<Footer year={2024} />
+
+<style>
+
+</style>
