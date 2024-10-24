78d580b366ffeda1e93c21a2c124f4eec7a6da6c
ZandarB <BEKEZA1@student.op.ac.nz>
2024-10-25 10:53:25 +1300

    changed error found with about page

--- disaster/disaster/src/routes/disasters/earthquakes/+page.svelte
+++ disaster/disaster/src/routes/disasters/earthquakes/+page.svelte
@@ -4,23 +4,25 @@
     import Image from '$lib/Image.svelte';
     import homepic from '$lib/Weather.jpg';
     let userInput = '';
+    let error = '';
+
 
 async function getData(number)
 {
     console.log(number);
     const response = await fetch(`http://api.geonet.org.nz/quake?MMI=-1`);
    
-    if (response.ok)
-    {
+   
+    if (response.ok) {
         const data = await response.json();
-        console.log(data.features[number]);
-        console.log(data.features[number].properties.mmi);
+        const feature = data.features[number];
 
-        
-    }
-    else
-    {
-        console.log("Error Fetching Data")
+        if (feature === undefined) {
+            error = "There was an error fetching your data, maybe the number input is invalid?";
+            console.log("Error Fetching Data");
+        } else {
+            const mmi = feature.properties.mmi;
+        }
     }
 }
 
@@ -34,17 +36,30 @@ function search()
 
 <Header headingTitle="Earthquakes" />
 
+
+<h3>Search for former Earthquakes</h3>
 <input
     id="textbox"
-    type="text"
+    type="number"
     bind:value={userInput}
-    placeholder="Please enter an Number"
+    placeholder="Please enter a Number"
 />
 <br>
 <button class="searchbutton" on:click={search}>Click to Search</button>
-
+<p class="error">{error}</p>
 <Footer year={2024} />
 
 <style>
 
+#textbox{
+    padding:10px
+}
+
+.searchbutton{
+    margin-top: 10px;
+}
+
+.error{
+    color: red;
+}
 </style>
