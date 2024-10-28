7e06a944c855334b94fa0e697b5cb8be003fb914
mapuhn1 <mapuhn1@student.op.ac.nz>
2024-10-28 14:32:09 +1300

    Add content to the how to use page

--- disaster/disaster/src/routes/howToUse/+page.svelte
+++ disaster/disaster/src/routes/howToUse/+page.svelte
@@ -7,6 +7,17 @@
 <Header headingTitle="How to use our website" />
 
 <article class = "container">
+    <h2> Explore Current and Recent Events</h2>
+  <div>
+    <p>Visit our Home Page to find information on natural disaster preparedness, response, and recovery.</p>
+    <p>Stay informed by checking the latest and warnings issued for your region in either the Current events or Recent Events Pages.</p>
+    <p>Visit the Disasters page to understand various natural disasters that can affect New Zealand, including earthquakes, floods, fires and more.</p>
+    <p>Find out more about us on the About us Page</p>
+    <p>Contact us on the Contact Page </p>
+
+
+</div>
+
 
 
 
