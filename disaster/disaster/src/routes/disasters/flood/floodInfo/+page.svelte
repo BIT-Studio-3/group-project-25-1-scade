cdd96ba0f0670e37ebd047a536fdc1b97f383bcc
Mapuhn1 <you@example.com>
2024-10-30 13:59:42 +1300

    key points

--- disaster/disaster/src/routes/disasters/flood/floodInfo/+page.svelte
+++ disaster/disaster/src/routes/disasters/flood/floodInfo/+page.svelte
@@ -6,6 +6,20 @@
 <Header headingTitle="Floods and your Safety" />
 
 
+<h2 id = "keyPoints">Key Points</h2>
+<ul>
+    <li>Floods, big or small, can have devastating effects on your home,your family and the general public. You can take steps to reduce the harm caused by flooding.</li>
+    <li>Learn what to inform the public on how to prepare for a flood, stay safe during a flood, and protect health after returning home from a flood</li>
+</ul>
+
+<h2>Prepare for a flood</h2>
+
+<!-- Add image here -->
+
 <style>
+
+    #keyPoints{
+        color: rgba(0, 68, 255, 0.658);
+    }
 
 </style>
