1458d47aeb47054f340a4c77586df68715b447b2
Mapuhn1 <you@example.com>
2024-10-30 13:27:01 +1300

    import header and footer

--- disaster/disaster/src/routes/disasters/flood/floodInfo/+page.svelte
+++ disaster/disaster/src/routes/disasters/flood/floodInfo/+page.svelte
@@ -1,7 +1,11 @@
 <script>
-
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte'
 </script>
 
+
+<Header headingTitle="Current Events" />
+<Footer year ={2024} />
 
 <style>
 
