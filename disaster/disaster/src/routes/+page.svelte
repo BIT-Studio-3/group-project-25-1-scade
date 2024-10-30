e25ce8e94ce79ad133426b2ebb3cc7dae8ed99ff
arnoao1 <jedifishcake@gmail.com>
2024-10-30 13:22:25 +1300

    corrected date system again, dd/mm/yyyy is now functional

--- disaster/disaster/src/routes/+page.svelte
+++ disaster/disaster/src/routes/+page.svelte
@@ -4,6 +4,8 @@
     import Image from '$lib/Image.svelte';
     import Footer from '$lib/Footer.svelte'
     import homepic from '$lib/Weather.jpg';
+    let currentTime = new Date();
+    let currentYear = currentTime.getFullYear();
     let day = String(currentTime.getDate()).padStart(2, '0');
     let month = String(currentTime.getMonth() + 1).padStart(2, '0'); 
     let year = currentTime.getFullYear(); 
@@ -16,7 +18,7 @@
         <h1><Header headingTitle="Current Events" /></h1>
         <p>{currentTime}</p>
         <div class="card-container">
-            <Card where="/disasters/earthquake" title="Earthquakes" date={currentYear} info="More about the Earthquakes" /> 
+            <Card where="/disasters/earthquake" title="Earthquakes" date={formattedDate} info="More about the Earthquakes" /> 
             <Card where="/disasters/fire" title="Fires" date={currentTime} info="More about the fires" />
             <Card where="/disasters/eruption" title="Eruptions" date={currentTime} info="More about the eruptions" />
             <Card where="/disasters/flood" title="Floods" date={currentTime} info="More about the floods" />
