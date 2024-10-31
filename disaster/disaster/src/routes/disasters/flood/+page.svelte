47d0884db61f5689e9b1a4b8909baf087f853175
Mapuhn1 <you@example.com>
2024-11-01 10:52:18 +1300

    add flood card component to flood page

--- disaster/disaster/src/routes/disasters/flood/+page.svelte
+++ disaster/disaster/src/routes/disasters/flood/+page.svelte
@@ -2,6 +2,7 @@
     import Header from '$lib/Header.svelte';
     import Image from '$lib/Image.svelte';
     import flood from '$lib/flood.jpg'
+    import Card from '$lib/Card.svelte'
 </script>
 
 <Header headingTitle="Floods" />
@@ -11,7 +12,14 @@
 <div class="imageContainor">
     <Image pic={flood} picname={"cover"} picwidth={"600px"} picheight={"400px"}/>
 </div>
-<a href="/disasters/flood/floodInfo">Floods and your safety</a>
+
+<Card
+   where ={"/disasters/flood/floodInfo"}
+   title ={"Floods and your Safety"}
+   date = 19/10/2024
+   info = "";
+></Card>
+
 
 
 <style>
