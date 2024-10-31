0ee46a4233f2c4323ed7c1c15f05f2d65667322e
SaskiaBLittle <briesg1@student.op.ac.nz>
2024-11-01 10:44:49 +1300

    placeholder card

--- disaster/disaster/src/routes/disasters/fire/+page.svelte
+++ disaster/disaster/src/routes/disasters/fire/+page.svelte
@@ -13,9 +13,15 @@
     <div class="float">
         <Card
         where ={"/disasters/fire/fireInfo"}
-        title ={"What are Wildfires ?"}
+        title ={"What are Wildfires?"}
         date = 19/10/2024
         info = "Wildfires are unplanned and uncontrolled fires that typically occur in wilderness areas..."
+        ></Card>
+        <Card
+        where ={"/"}
+        title ={"Event"}
+        date = 19/10/2024
+        info = "Recent fire event in 'Location' at 'time', placeholder for now"
         ></Card>
     </div>
     <div class="float">
