51b94c4090970a885462f963ed6bd68cfc5e7c08
SaskiaBLittle <briesg1@student.op.ac.nz>
2024-11-01 10:40:43 +1300

    sit side by side, but eventually have a bug

--- disaster/disaster/src/routes/disasters/fire/+page.svelte
+++ disaster/disaster/src/routes/disasters/fire/+page.svelte
@@ -3,25 +3,31 @@
     import Image from '$lib/Image.svelte';
     import Card from '$lib/Card.svelte'
     import fire from '$lib/fire.jpg';
-    
-  
 </script>
 
 <Header headingTitle="Fires" />
 
 <p>Fires</p>
 
-<div class="imageContainor">
-    <Image pic={fire} picname={"cover"} picwidth={"600px"} picheight={"400px"}/>
+<div>
+    <div class="block">
+        <Image pic={fire} picname={"cover"} picwidth={"600px"} picheight={"400px"}/>
+    </div>
+
+    <div class="block">
+        <Card
+        where ={"/disasters/fire/fireInfo"}
+        title ={"What are Wildfires ?"}
+        date = 19/10/2024
+        info = "Wildfires are unplanned and uncontrolled fires that typically occur in wilderness areas..."
+        ></Card>
+    </div>
 </div>
 
-<Card
-   where ={"/disasters/fire/fireInfo"}
-   title ={"What are Wildfires ?"}
-   date = 19/10/2024
-   info = "Wildfires are unplanned and uncontrolled fires that typically occur in wilderness areas..."
-></Card>
+
 
 <style>
-
+    .block {
+        display: inline-block;
+    }
 </style>
