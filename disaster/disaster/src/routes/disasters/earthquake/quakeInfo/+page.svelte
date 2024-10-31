18477fa493bc2d8eaa464f37bbd7b0c85d9f4bc8
Joanna Marowa <marojm1@student.op.ac.nz>
2024-11-01 10:39:07 +1300

    Images added

--- disaster/disaster/src/routes/disasters/earthquake/quakeInfo/+page.svelte
+++ disaster/disaster/src/routes/disasters/earthquake/quakeInfo/+page.svelte
@@ -1,6 +1,9 @@
 <!--Earthquake info -->
 <script>
-
+ import Header from "$lib/Header.svelte";
+ import Image from "$lib/Image.svelte";
+ import faults from "$lib/faults.jpg";
+ import quake from "$lib/quake.jpg"
 </script>
 
 <div></div>
