76ff70d56767dc8a1f649b6eeb47cfb2e0312051
ZandarB <BEKEZA1@student.op.ac.nz>
2024-11-01 10:25:02 +1300

    started flood API

--- disaster/disaster/src/routes/disasters/flood/+page.svelte
+++ disaster/disaster/src/routes/disasters/flood/+page.svelte
@@ -2,6 +2,27 @@
     import Header from '$lib/Header.svelte';
     import Image from '$lib/Image.svelte';
     import flood from '$lib/flood.jpg'
+
+    const apiKey = "gvzqb8DKRo8iVxK38YHq5Psak0iT09Jq"
+
+async function getData()
+{
+    const response = await fetch(`https://api.niwa.co.nz/tides/data`, 
+        {
+        headers: {
+            'x-apikey': apiKey
+        
+        }});
+
+    tideData = await response.json();
+
+
+}
+
+
+getData();
+
+
 </script>
 
 <Header headingTitle="Floods" />
