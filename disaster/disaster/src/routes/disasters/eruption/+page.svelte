0d6de101d828a3079bcf956fbd34c4b1e2e7f594
Mapuhn1 <you@example.com>
2024-11-06 14:02:02 +1300

    add card to page

--- disaster/disaster/src/routes/disasters/eruption/+page.svelte
+++ disaster/disaster/src/routes/disasters/eruption/+page.svelte
@@ -2,6 +2,7 @@
     import Header from '$lib/Header.svelte';
     import Image from '$lib/Image.svelte';
     import volcano from '$lib/volcano.jpg'
+    import Card from '$lib/Card.svelte';
 
     let searchInput = "";
     let errorMessage = "No volcanoes found matching your search."
@@ -24,7 +25,24 @@
 
 <Header headingTitle="Volcanic Eruptions" />
 
-<p>Volcanic Eruptions</p>
+<div>
+    <div class="float">
+        <Card
+        where ={"/disasters/eruption/eruptionInfo"}
+        title ={"What are Volcanoes?"}
+        date = 19/10/2024
+        info = "Volcanoes are "
+        ></Card>
+        <Card
+        where ={"/"}
+        title ={"Event"}
+        date = 19/10/2024
+        info = "Recent fire event in 'Location' at 'time', placeholder for now"
+        ></Card>
+    </div>
+</div>
+
+
 
 <div class="imageContainor">
     <Image pic={volcano} picname={"cover"} picwidth={"600px"} picheight={"400px"}/>
---
+++ disaster/disaster/src/routes/disasters/eruption/eruptionInfo/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Header from '$lib/Header.svelte';
+      import Image from '$lib/Image.svelte';
+    import flood from '$lib/flood.jpg'
+    
+</script>
