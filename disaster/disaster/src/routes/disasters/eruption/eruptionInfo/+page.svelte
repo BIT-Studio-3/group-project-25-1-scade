f88b9cd12d73ad296a3def3244459af67f27dc97
Mapuhn1 <you@example.com>
2024-11-06 14:07:33 +1300

    add header

--- disaster/disaster/src/routes/disasters/eruption/eruptionInfo/+page.svelte
+++ disaster/disaster/src/routes/disasters/eruption/eruptionInfo/+page.svelte
@@ -1,6 +1,16 @@
 <script>
     import Header from '$lib/Header.svelte';
-      import Image from '$lib/Image.svelte';
-    import flood from '$lib/flood.jpg'
+    import Image from '$lib/Image.svelte';
+    import flood from '$lib/volcano.jpg'
     
 </script>
+<Header headingTitle = "What are volcanoes" />
+
+<div class="imageContainor">
+    <Image
+      pic={volcano}
+      picname={"cover"}
+      picwidth={"500px"}
+      picheight={"300px"}
+    />
+  </div>
