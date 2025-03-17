9457c9a14aabf6da09c050c1418cf450f54217b0
Aston Arnott (1000122523) <ARNOAO1@student.op.ac.nz>
2025-03-17 15:48:17 +1300

    update(about): moved contact page form functionality into about page, making contact page redundant

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -84,7 +84,47 @@
   </div>
 </div>
 <br />
+<form action="">
+  <label for="email"
+    >Email
+    <input type="email" id="email" placeholder="Your email.." required />
+  </label>
 
+  <label for="First name">
+    First Name
+    <input type="text" id="fname" placeholder="Your name.." required />
+  </label>
+
+  <label for="Last name">
+    Last Name
+    <input type="text" id="lname" placeholder="Your  last name.." required />
+  </label>
+
+  <label for="region">Region</label>
+  <select>
+    <option value="auckland">Auckland</option>
+    <option value="Bay of Plenty">Bay of Plenty</option>
+    <option value="northland">Northland</option>
+    <option value="Waikato">Waikato</option>
+    <option value="Canterbury">Canterbury</option>
+    <option value=" Gisborne">Gisborne</option>
+    <option value="Hawke's Bay">Hawke's Bay</option>
+    <option value="Manawatū-Whanganui">Manawatū-Whanganui</option>
+    <option value="Marlborough">Marlborough</option>
+    <option value="Nelson">Nelson</option>
+    <option value="Otago">Otago</option>
+    <option value="Southland">Southland</option>
+    <option value="Taranaki">Taranaki</option>
+    <option value="Tasman">Tasman</option>
+    <option value="Wellington">Wellington</option>
+    <option value="West Coast">West Coat</option>
+  </select>
+
+  <label for="message">Your message / Feedback</label>
+  <textarea name="message" id="message" required></textarea>
+
+  <input type="submit" value="Submit" />
+</form>
 <style>
   .members {
     display: inline-block;
@@ -108,5 +148,50 @@
   }
   *{
     margin: 10px;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    padding: 12px;
+  }
+
+  input[type="text"],
+  input[type="email"],
+  textarea,
+  select {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+  }
+  input[type="submit"] {
+    background-color: rgb(40, 51, 122);
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  input[type="submit"]:hover {
+    background-color: rgb(57, 72, 167);
+  }
+
+  input:invalid:required,
+  textarea:invalid:required {
+    background-color: ivory;
+    border: none;
+    outline: 2px solid rgb(40, 51, 122);
+    border-radius: 5px;
+  }
+
+  @media screen and (max-width: 600px) {
+    input[type="submit"] {
+      width: 100%;
+      margin-top: 0;
+    }
   }
 </style>
