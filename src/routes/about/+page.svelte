ed1656788bc61da8488a1d48739e2948c524402a
Aston Arnott (1000122523) <ARNOAO1@student.op.ac.nz>
2025-03-17 16:52:41 +1300

    feature(about):added basic form for user to enter their email and text for a message

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,8 @@
 <script>
   import Header from "$lib/Header.svelte";
   import Image from "$lib/Image.svelte";
-  import homepic from "$lib/Weather.jpg";
+  let email = "";
+  let message = "";
 </script>
 
 <Header headingTitle="About" />
@@ -17,10 +18,32 @@ The lack of a pre-existing system within the New Zealand government inspired us 
 </p>
 
 <p>If you wish to contact us, please use the contact form below.</p>
-
+<form method="POST">
+	<label>
+		Your Email
+		<input name="email" type="email">
+	</label>
+  <br>
+	<label>
+		Your Message
+		<textarea name="message" bind:value={message} placeholder="Enter your message here..."></textarea>
+	</label>
+	<button>Submit</button>
+</form>
 
 <style>
   p {
   margin-bottom: 20px;
 }
+  form{
+    margin-bottom: 20px;
+    display: flexbox;
+  }
+  .email{
+    margin-bottom: 20px;
+  }
+  .message{
+    margin-bottom: 20px;
+  }
+  
 </style>
