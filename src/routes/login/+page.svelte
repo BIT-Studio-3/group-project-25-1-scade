aa60acbf9c6ee769cf4ee99bf28c9613a5963ee1
Saskia Brienesse-Little (1000125002) <BRIESG1@student.op.ac.nz>
2025-03-28 11:30:55 +1300

    Login page CSS mostly made, still some bugs and layout problem

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,120 @@
+<script>
+
+</script>
+
+<div class="container">
+    <div class="title">
+        <h1>SCADE</h1>
+    </div>
+
+    <div class="right">
+        <div class="login-box">
+            <h2>Login</h2>
+            <input type="text" placeholder="Username" required />
+            <input type="password" placeholder="password" required />
+            <button type="submit">Log In</button>
+        </div>
+    </div>
+</div>
+
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .container {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        flex-wrap: wrap;
+    }
+    .title {
+        background-color: rgb(40, 51, 122);
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        text-align: center;
+        padding: 20px;
+    }
+    .right {
+        flex: 1;
+        background-color: aliceblue;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+    }
+    .login-box {
+        width: 100%;
+        max-width: 400px;
+        padding: 20px;
+        background-color: white;
+        border: 2px solid black;
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        border-radius: 5px;
+    }
+    .login-box input {
+        width: 100%;
+        padding: 10px;
+        margin: 10px 0;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        font-size: 1rem;
+    }
+    .login-box button {
+        width: 100%;
+        padding: 10px;
+        background-color: rgb(40, 51, 122);
+        color: white;
+        border: none;
+        border-radius: 5px;
+        font-size: 1rem;
+        cursor: pointer;
+    }
+    .login-box button:hover {
+        background-color: rgb(30, 41, 92);
+    }
+
+    h1 {
+        color: white;
+        font-family: "Times New Roman", Times, serif;
+        font-weight: lighter;
+        font-size: 5rem;
+        margin: 0;
+    }
+
+    /* Mobile phone Design */
+    @media (max-width: 768px) {
+      .container {
+        flex-direction: column;
+      }
+
+      .right h1 {
+        font-size: 3rem;
+      }
+
+      .left {
+        flex: none;
+        width: 100%;
+        padding: 15px;
+      }
+
+      .login-box {
+        max-width: 350px;
+        margin: 0 auto;
+      }
+
+      .right {
+        flex: none;
+        width: 100%;
+        padding: 20px;
+        order: -1;
+      }
+    }
+</style>
