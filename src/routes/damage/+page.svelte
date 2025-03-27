6b56d907b2b49f62a9aeda06ec68ce50ccc07aea
Edward Wright (1000126272) <WRIGEG1@student.op.ac.nz>
2025-03-28 11:29:47 +1300

    added table

--- src/routes/damage/+page.svelte
+++ src/routes/damage/+page.svelte
@@ -10,14 +10,29 @@
   let damages = [
     { damage: 'This and that is damaged', location: '123 Paper St'},
     { damage: 'that is damaged and this is not', location: '321 Paper St'}
-
   ];
 </script>
 
 <body>
   <div class="top-of-body">
     <h2><Header headingTitle="Damage Records" /></h2>
-   
+    
+    <table>
+      <thead>
+        <tr>
+          <th>Damage</th>
+          <th>Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each damages as { damage, location}}
+          <tr>
+            <td>{damage}</td>
+            <td>{location}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
   
   </div>
 </body>
@@ -33,6 +48,25 @@
   .top-of-body {
     border-style: solid;
     border-color: grey;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  
+  th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+
+  tr:nth-child(even) {
+    background-color: #f9f9f9;
   }
   
 </style>
