d9a284e63e7820bcb1120ab3f1a6fcb4ddf3e969
Edward Wright (1000126272) <WRIGEG1@student.op.ac.nz>
2025-04-04 10:23:36 +1300

    removeded more unused code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,6 @@
   import { onMount } from 'svelte';
   import IndexCard from '$lib/indexcard.svelte'; // Import the IndexCard component
   let earthquakeData = {};  // This will hold the data from the child component
-
-  let currentTime = new Date();
-  let currentYear = currentTime.getFullYear();
-  let day = String(currentTime.getDate()).padStart(2, '0');
-  let month = String(currentTime.getMonth() + 1).padStart(2, '0'); 
-  let year = currentTime.getFullYear(); 
-  let formattedDate = `${day}/${month}/${year}`;
   
 </script>
 
@@ -46,39 +39,6 @@
 
   </section>
   
-
-  <!-- <div class="top-of-body">
-    <h2><Header headingTitle="Current Events" /></h2>
-    <p>{currentTime}</p>
-    <div class="current-events">
-      <Maincard class="earthquake-card" where="/disasters/earthquake" title="Earthquakes" date={formattedDate} info="More about the Earthquakes" />
-      <Maincard where="/disasters/fire" title="Fires" date={formattedDate} info="More about the fires" />
-      <Maincard where="/disasters/eruption" title="Eruptions" date={formattedDate} info="More about the eruptions" />
-      <Maincard where="/disasters/flood" title="Floods" date={formattedDate} info="More about the floods" />
-    </div>
-  </div>
-
-
-
-  <div class="bottom-of-body">
-    <h1>&numsp;Recent Events</h1>
-    <div class="recent-events">
-      <Maincard class="earthquakeCard" where="/disasters/earthquake" title="Earthquakes" date="18/10/24" info="More about the Earthquakes"location={location}>
-        <p><strong>Location:</strong> {location}</p>
-        <p><strong>Magnitude:</strong> {magnitude}</p>
-        <p><strong>Depth:</strong> {depth} km</p>
-        <p><strong>MMI:</strong> {mmi}</p>
-        <p><strong>Time:</strong> {quakeTime}</p>  
-      </Maincard> 
-      <Maincard where="/disasters/fire" title="Fires" date="18/10/24" info="More about the fires" />
-      <Maincard where="/disasters/eruption" title="Eruptions" date="18/10/24" info="More about the eruptions" />
-      <Maincard where="/disasters/flood" title="Floods" date="18/10/24" info="More about the floods">
-        <p>testtestest</p>
-      </Maincard>
-    </div>
-
-    
-  </div> -->
 </body>
 
 
@@ -107,12 +67,6 @@
     }
   
 
-  #indexCard1{
-    grid-row: span 2/ span 2;
-    height: 94%;
- 
-  }
-
   #indexCard4{grid-row-start: 3;}
   #indexCard5{grid-row-start: 3;}
   #indexCard6{grid-column: span 2 / span 2;
@@ -120,40 +74,5 @@
     grid-row-start: 4;}
 
 
-  /* p {
-    padding: 1.2%;
-  }
-  h2 {
-    padding: 0.9%;
-  }
-  h1 {
-    padding: 0.2%;
-  }
-  .top-of-body {
-    border-style: solid;
-    border-color: grey;
-  }
-  .bottom-of-body {
-    border-style: solid;
-    border-color: grey;
-  }
-  .current-events, .recent-events {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    gap: 20px;
-    padding: 10px;
-  }
-  .earthquake-card {
-    width: 300px;
-  }
-  .quake-info {
-    margin-top: 20px;
-  }
-  .imageContainor {
-    display: flex;
-    justify-content: right;
-    padding: 2%;
-  } */
-
+ 
 </style>
