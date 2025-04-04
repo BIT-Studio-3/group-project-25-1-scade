fdefe06ef77a2a4a4047d7644f08dec6076fe436
Daniel Morris (1000112241) <MORRDJ1@student.op.ac.nz>
2025-04-04 14:44:35 +1300

    doc(login): added comments and white space to login js

--- src/routes/(login)/login/+page.server.js
+++ src/routes/(login)/login/+page.server.js
@@ -1,17 +1,24 @@
+//todo
+//instead of checking if the user cookie exists check it against a list of hard coded users and passwords
+
 import { redirect } from "@sveltejs/kit";
 
 export const actions = {
     login: async ({ request, cookies }) => {
 
+        //pulls form data and saves them as variables
         const data = await request.formData();
         const username = data.get('username');
         const password = data.get('password');
 
+        //sets the variables as cookies
         cookies.set('user', username , { path: '/', maxAge: 86400 });
 
+        //checks if the user variable exists
         if (cookies.get('user')) {
             redirect(303, '/');
         }
+        
         return { success: true };
     }
 };
