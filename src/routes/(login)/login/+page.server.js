792004df4c9d275a79f99f552daed93099f7fb25
Daniel Morris (1000112241) <MORRDJ1@student.op.ac.nz>
2025-04-04 14:38:36 +1300

    fix(login): added redirect to home page after logging in

--- src/routes/(login)/login/+page.server.js
+++ src/routes/(login)/login/+page.server.js
@@ -2,13 +2,16 @@ import { redirect } from "@sveltejs/kit";
 
 export const actions = {
     login: async ({ request, cookies }) => {
+
         const data = await request.formData();
         const username = data.get('username');
         const password = data.get('password');
-        console.log(JSON.stringify({ username }));
-        let name = username;
-        
+
         cookies.set('user', username , { path: '/', maxAge: 86400 });
+
+        if (cookies.get('user')) {
+            redirect(303, '/');
+        }
         return { success: true };
     }
 };
