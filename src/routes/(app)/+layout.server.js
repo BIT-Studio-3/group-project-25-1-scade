4fe6d5167d5ac9639b845149b381f36f08e79ec3
Saskia Brienesse-Little (1000125002) <BRIESG1@student.op.ac.nz>
2025-05-23 10:43:43 +1200

    Page Authentication updated

--- src/routes/(app)/+layout.server.js
+++ src/routes/(app)/+layout.server.js
@@ -2,8 +2,14 @@ import { redirect } from "@sveltejs/kit";
 
 export async function load({ cookies }) {
     const user = cookies.get("token");
+
+    const statement = database.prepare('SELECT * FROM users WHERE token = ?');
+    const users = statement.get(token);
     
     if (!user) {
+        throw redirect(303, "/login");
+    }
+    if (!users) {
         throw redirect(303, "/login");
     }
 }
